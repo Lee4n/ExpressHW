1f3893cf12a414c737f1dffda816bb713256eb67
Lee Lawler <lawler_lee@icloud.com>
2020-03-19 05:29:55 -0500

    updated survey.html with styling and input form

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
 app.listen(PORT, function () {
     console.log(`App listening on http://localhost:${PORT}`);
 });
