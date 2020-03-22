d5ef520293f619693fab9aadd786b3fd546707d2
Lee Lawler <lawler_lee@icloud.com>
2020-03-21 23:28:08 -0500

    added friend matching data / currently bugged

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,50 @@
 var friends = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}]
+        "name": "Jerome",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }, {
+        "name": "Jeffrey E. Pstein",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            4,
+            4,
+            4,
+            5,
+            4,
+            1,
+            2,
+            3,
+            4
+        ]
+    }, {
+        "name": "Lucy",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            5,
+            3,
+            5,
+            5,
+            4,
+            5,
+            1,
+            2,
+            5
+        ]
+    }
+
+]
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,48 @@
 var friendsData = require("../data/friends");
 
-module.exports = function(app) {
-    
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
         friendsData.push(req.body);
         res.json(true);
-    })
-}
+
+        var userData = req.body;
+
+        var scores = friendsData.map(function (item) {
+            return parseInt(item, 10);
+        });
+
+        var match = {
+            name: "",
+            photo: "",
+            difference: 1000
+        };
+
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: scores
+        };
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var totalDifference = 0;
+            var sum = scores.reduce((a, b) => a + b, 0);
+            var matchScore = friendsData[i].scores.reduce((a, b) => a + b, 0);
+            totalDifference += Math.abs(sum - matchScore);
+
+            if (totalDifference <= match.difference) {
+                match.name = friendsData[i].name;
+                match.photo = friendsData[i].photo;
+                match.difference = totalDifference;
+            }
+        };
+
+        friendsData.push(userData);
+
+        res.json(match);
+    });
+};
